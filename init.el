a0b4259a40b67a1fb5b5f01bc1e3d7aa8828b2cf
Samuel Almeida <samuel@bravion.dev>
2022-02-19 20:01:05 -0300

    Added solidity mode

--- init.el
+++ init.el
@@ -72,7 +72,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages '(magit counsel-projectile projectile ivy use-package)))
+ '(package-selected-packages
+   '(solidity-mode magit counsel-projectile projectile ivy use-package)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -147,3 +148,8 @@
 
 (use-package magit)
 
+;; Programming Languages
+
+;; Solidity
+
+(use-package solidity-mode)
